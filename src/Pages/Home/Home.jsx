5a54a98f88017270d7e4903b669a7a24a051e1f2
Maruf <maruf.khann931@gmail.com>
2025-06-25 21:57:29 +0530

    Added Navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,6 @@ function Home(){
     return(
         <div>
             <Navbar />
-            <h1>Home Page</h1>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
-import './Navbar.css'
+import './Navbar.css';
+import logo from "../../assets/assets/logo.png";
+import search_icon from "../../assets/assets/search_icon.svg";
+import bell_icon from "../../assets/assets/bell_icon.svg";
+import profile from "../../assets/assets/profile_img.png";
+import caret_icon from "../../assets/assets/caret_icon.svg";
 
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <div>
-            <navbar>
-                <ul>
-                    <li>Home</li>
-                    <li>Contact</li>
-                    <li>About us</li>
-                    <li>Know More</li>
-                </ul>
-            </navbar>
+            <div className='navbar'>
+                <div className='navbar-left'>
+                    <img src={logo} alt="Logo" />
+                    <ul>
+                        <li>Home</li>
+                        <li>TV Shows</li>
+                        <li>Movies</li>
+                        <li>New & Popular</li>
+                        <li>My List</li>
+                        <li>Browse by language</li>
+                    </ul>
+                </div>
+                <div className='navbar-right'>
+                    <img src={search_icon} alt="search_icon" className='icons' />
+                    <p>Children</p>
+                    <img src={bell_icon} alt="bell_icon" className='icons' />
+                    <div className='navbar-profile'>
+                        <img src={profile} alt="profile" className='profile' />
+                        <img src={caret_icon} alt="caret_icon" />
+                        <div className='dropDown'>
+                            <p>Sign out of Netflix</p>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
     )
 }
